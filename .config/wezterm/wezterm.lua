178f73a9d0c2a8ce49192996f598ce48a173768f
hyblm <hyma.code@gmail.com>
2022-07-24 17:59:56 +0200

    add wezterm config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,15 @@
+local wezterm = require("wezterm")
+
+return {
+	color_scheme = "Gruvbox Dark",
+  -- color_scheme = "Gruvbox Dark",
+	font_size = 12,
+	font = wezterm.font( "Fira Code" ),
+	harfbuzz_features = { "zero", "cv14", "cv31", "ss03", "ss02" },
+	enable_tab_bar = false,
+	window_background_opacity = 0.95,
+	window_padding = {
+		top = 0,
+		bottom = 0,
+	},
+}
