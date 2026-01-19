2f8863e4b3be326697f85135b359dddf869a73d8
Matyáš Hýbl <hyma.code@gmail.com>
2026-01-19 09:49:06 +0100

    move neovide config to options file

--- nvim/lua/config/neovide.lua
+++
@@ -1,4 +0,0 @@
-if vim.g.neovide == true then
-  vim.o.guifont = "Lilex Nerd Font:h11.4" -- the biggest that fits 2 full width views side by side
-  vim.api.nvim_set_keymap("n", "<F11>", ":let g:neovide_fullscreen = !g:neovide_fullscreen<CR>", {})
-end
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,3 +3,8 @@
 -- Add any additional options here
 vim.opt.relativenumber = false
 vim.opt.number = false
+
+if vim.g.neovide == true then
+  vim.o.guifont = "Lilex Nerd Font:h11.4" -- the biggest that fits 2 full width views side by side
+  vim.api.nvim_set_keymap("n", "<F11>", ":let g:neovide_fullscreen = !g:neovide_fullscreen<CR>", {})
+end
