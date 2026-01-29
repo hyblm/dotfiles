a13c0d72de40dbe1b2f805cc7993540fe69f22d0
Matyáš Hýbl <hyma.code@gmail.com>
2026-01-29 15:22:10 +0100

    cleanup

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -6,5 +6,7 @@ vim.opt.number = false
 
 if vim.g.neovide == true then
   vim.o.guifont = "Lilex Nerd Font:h11.4" -- the biggest that fits 2 full width views side by side
+  vim.g.neovide_remember_window_size = true
+  vim.g.neovide_remember_window_position = true
   vim.api.nvim_set_keymap("n", "<F11>", ":let g:neovide_fullscreen = !g:neovide_fullscreen<CR>", {})
 end
