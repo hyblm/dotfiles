2d2deb886583fe54340711e9f57c9b428edbfbe8
Matyas Hybl <hyblm@proton.me>
2026-05-08 13:40:26 +0200

    adjust tabstop

--- nvim/lua/hyblm/options.lua
+++ nvim/lua/hyblm/options.lua
@@ -6,9 +6,9 @@
 vim.o.undofile = true
 vim.o.smoothscroll = true
 vim.o.expandtab = true
+
 vim.o.shiftwidth = 4
-vim.o.softtabstop = 2
-vim.o.tabstop = 2
+vim.o.tabstop = 4
 vim.o.ignorecase = true -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.o.smartcase = true
 vim.o.cursorline = true -- Highlight the line where the cursor is on.
@@ -36,9 +36,9 @@ require('vim._core.ui2').enable()
 -- increase startup-time. Remove this option if you want your OS clipboard to remain independent.
 -- See `:h 'clipboard'`
 vim.api.nvim_create_autocmd('UIEnter', {
-  callback = function()
-    vim.o.clipboard = 'unnamedplus'
-  end,
+    callback = function()
+        vim.o.clipboard = 'unnamedplus'
+    end,
 })
 
 -- AUTOCOMMANDS (EVENT HANDLERS)
@@ -48,10 +48,10 @@ vim.api.nvim_create_autocmd('UIEnter', {
 -- Highlight when yanking (copying) text.
 -- Try it with `yap` in normal mode. See `:h vim.hl.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  callback = function()
-    vim.hl.on_yank()
-  end,
+    desc = 'Highlight when yanking (copying) text',
+    callback = function()
+        vim.hl.on_yank()
+    end,
 })
 
 -- USER COMMANDS: DEFINE CUSTOM COMMANDS
@@ -60,9 +60,9 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 
 -- Create a command `:GitBlameLine` that print the git blame for the current line
 vim.api.nvim_create_user_command('GitBlameLine', function()
-  local line_number = vim.fn.line('.') -- Get the current line number. See `:h line()`
-  local filename = vim.api.nvim_buf_get_name(0)
-  print(vim.system({ 'git', 'blame', '-L', line_number .. ',+1', filename }):wait().stdout)
+    local line_number = vim.fn.line('.') -- Get the current line number. See `:h line()`
+    local filename = vim.api.nvim_buf_get_name(0)
+    print(vim.system({ 'git', 'blame', '-L', line_number .. ',+1', filename }):wait().stdout)
 end, { desc = 'Print the git blame for the current line' })
 
 -- PLUGINS
