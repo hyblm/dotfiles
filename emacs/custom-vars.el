4a9cae3a5d7130095cb24a08a66750492ade64d9
Matyas Hybl <hyblma.code@gmail.com>
2026-08-16 14:07:53 +0200

    emacs initial

---
+++ emacs/custom-vars.el
@@ -0,0 +1,18 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("acfe7ff6aacb9432f124cde4e35d6d2b4bc52916411de73a6ccded9750c9fa97"
+     "b8bd60a23b9e2f08b0c437231ee84f2dacc70fdc4d5a0fb87229bb9926273fdd"
+     "4d66d185c52e429e814f98265ee34b314bf0ea21a9c0bd020ef406e9f37c15a6"
+     default))
+ '(package-selected-packages '(eat exwm magit meow tao-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
