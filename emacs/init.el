ec0f393f1cdfcb100d28755165318cbe1faac804
Matyáš Hýbl <hyblm@proton.me>
2026-09-09 20:32:24 +0200

    better organized ligatures config

--- emacs/init.el
+++ emacs/init.el
@@ -63,21 +63,30 @@ The DWIM behaviour of this command is as follows:
 (set-face-attribute 'variable-pitch nil :font "Atkinson Hyperlegible Next" :weight 'normal :height 1.2)
 
 (use-package ligature
-  :ensure t
   :config
-  (ligature-set-ligatures
-   'prog-mode
-   '("&&" "***" "**" "*/" "/*" "//" "///" "||" "|="
-     "|>" "<|" "->" "-->" "<-" "<--" "<->" "=>" "==>" "<=="
-     "<=>" "!=" "!==" "==" "===" "=~" "!~" "<=" ">="
-     "++" "+++" "--" "---" "+=" "-=" "*=" "/=" "%=" "&=" "^=" ":=" "::"
-     ".." "..." "..=" ";;" "!!" "??" "?." "::=" "<~" "~>"
-     "<*" "*>" "<*>" "<$" "$>" "<$>" "<+>" "<|>" "<>"
-     "#(" "#{" "#!" ".=" ".<" ".?"  ":::"
-     "</" "</>" "/>" "<<" "<<<" "/**" "/*" "#[" "]#"
-     "{-" "-}" "{|" "|}" "[|" "|]" "(#" "#)" "##" "###"
-     "####" "%%" "%%%" "~~" "~=" "^=" "\\\\"))
+  ;; Enable the "www" ligature in every possible major mode
+  (ligature-set-ligatures 't '("www"))
+  ;; Enable traditional ligature support in eww-mode, if the
+  ;; `variable-pitch' face supports it
+  (ligature-set-ligatures 'eww-mode '("ff" "fi" "ffi"))
+  ;; Enable all Cascadia Code ligatures in programming modes
+  (ligature-set-ligatures 'prog-mode '("|||>" "<|||" "<==>" "<!--" "####" "~~>" "***" "||=" "||>"
+                                       ":::" "::=" "=:=" "===" "==>" "=!=" "=>>" "=<<" "=/=" "!=="
+                                       "!!." ">=>" ">>=" ">>>" ">>-" ">->" "->>" "-->" "---" "-<<"
+                                       "<~~" "<~>" "<*>" "<||" "<|>" "<$>" "<==" "<=>" "<=<" "<->"
+                                       "<--" "<-<" "<<=" "<<-" "<<<" "<+>" "</>" "###" "#_(" "..<"
+                                       "..." "+++" "/==" "///" "_|_" "www" "&&" "^=" "~~" "~@" "~="
+                                       "~>" "~-" "**" "*>" "*/" "||" "|}" "|]" "|=" "|>" "|-" "{|"
+                                       "[|" "]#" "::" ":=" ":>" ":<" "$>" "==" "=>" "!=" "!!" ">:"
+                                       ">=" ">>" ">-" "-~" "-|" "->" "--" "-<" "<~" "<*" "<|" "<:"
+                                       "<$" "<=" "<>" "<-" "<<" "<+" "</" "#{" "#[" "#:" "#=" "#!"
+                                       "##" "#(" "#?" "#_" "%%" ".=" ".-" ".." ".?" "+>" "++" "?:"
+                                       "?=" "?." "??" ";;" "/*" "/=" "/>" "//" "__" "~~" "(*" "*)"
+                                       "\\\\" "://"))
+  ;; Enables ligature checks globally in all buffers. You can also do it
+  ;; per mode with `ligature-mode'.
   (global-ligature-mode t))
+
 
 
 (defun my/system-dark-mode-p ()
