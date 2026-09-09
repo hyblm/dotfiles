4906d1febc6d936fe19c77956f380a7dd4577489
Matyáš Hýbl <hyblm@proton.me>
2026-09-09 19:55:00 +0200

    literate config

---
+++ emacs/custom.el
@@ -0,0 +1,18 @@
+;;; -*- lexical-binding: t -*-
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(consult-todo corfu doric-themes eat exwm magit-todos marginalia meow
+		  nerd-icons nerd-icons-corfu nerd-icons-dired
+		  orderless tao-theme vertico)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
--- emacs/init.el
+++ emacs/init.el
@@ -1,6 +1,9 @@
-;;; -*- lexical-binding: t; -*-
+;;; init.el -*- lexical-binding: t; -*-
 
-;; Bootstrap straight.el before declaring packages.
+(setq custom-file (locate-user-emacs-file "custom.el"))
+(load custom-file :no-error-if-file-is-missing)
+
+
 (let* ((straight-base (expand-file-name "straight/" user-emacs-directory))
        (straight-dir (expand-file-name "repos/straight.el" straight-base))
        (bootstrap-file (expand-file-name "bootstrap.el" straight-dir)))
@@ -14,6 +17,68 @@
 (straight-use-package 'use-package)
 (setq straight-use-package-by-default t)
 (eval-when-compile (require 'use-package))
+
+
+(add-to-list 'display-buffer-alist
+             '("\\`\\*\\(Warnings\\|Compile-Log\\)\\*\\'"
+               (display-buffer-no-window)
+	       (allow-no-window . t)))
+
+
+
+(use-package delsel
+  :ensure nil
+  :straight (:type built-in)
+  :hook (after-init . delete-selection-mode))
+
+(defun prot/keyboard-quit-dwim ()
+  "Do-What-I-Mean behaviour for a general `keyboard-quit'.
+
+The generic `keyboard-quit' does not do the expected thing when
+the minibuffer is open.  Whereas we want it to close the
+minibuffer, even without explicitly focusing it.
+
+The DWIM behaviour of this command is as follows:
+
+- When the region is active, disable it.
+- When a minibuffer is open, but not focused, close the minibuffer.
+- When the Completions buffer is selected, close it.
+- In every other case use the regular `keyboard-quit'."
+  (interactive)
+  (cond
+   ((region-active-p)
+    (keyboard-quit))
+   ((derived-mode-p 'completion-list-mode)
+    (delete-completion-window))
+   ((> (minibuffer-depth) 0)
+    (abort-recursive-edit))
+   (t
+    (keyboard-quit))))
+
+(define-key global-map (kbd "C-g") #'prot/keyboard-quit-dwim)
+
+
+(set-face-attribute 'default        nil :font "Lilex Nerd Font"            :weight 'normal :height 110)
+(set-face-attribute 'fixed-pitch    nil :font "Lilex Nerd Font"            :weight 'normal :height 1.0)
+(set-face-attribute 'variable-pitch nil :font "Atkinson Hyperlegible Next" :weight 'normal :height 1.2)
+
+(use-package ligature
+  :ensure t
+  :config
+  (ligature-set-ligatures
+   'prog-mode
+   '("&&" "***" "**" "*/" "/*" "//" "///" "||" "|="
+     "|>" "<|" "->" "-->" "<-" "<--" "<->" "=>" "==>" "<=="
+     "<=>" "!=" "!==" "==" "===" "=~" "!~" "<=" ">="
+     "++" "+++" "--" "---" "+=" "-=" "*=" "/=" "%=" "&=" "^=" ":=" "::"
+     ".." "..." "..=" ";;" "!!" "??" "?." "::=" "<~" "~>"
+     "<*" "*>" "<*>" "<$" "$>" "<$>" "<+>" "<|>" "<>"
+     "#(" "#{" "#!" ".=" ".<" ".?"  ":::"
+     "</" "</>" "/>" "<<" "<<<" "/**" "/*" "#[" "]#"
+     "{-" "-}" "{|" "|}" "[|" "|]" "(#" "#)" "##" "###"
+     "####" "%%" "%%%" "~~" "~=" "^=" "\\\\"))
+  (global-ligature-mode t))
+
 
 (defun my/system-dark-mode-p ()
   "Return non-nil when the desktop is currently using a dark theme."
@@ -34,15 +99,123 @@
       (mapc #'disable-theme custom-enabled-themes)
       (load-theme theme t))))
 
-;; Make local themes and their shared definitions discoverable.
+;;;; Make local themes and their shared definitions discoverable.
 (let ((theme-dir (expand-file-name "themes" user-emacs-directory)))
   (add-to-list 'custom-theme-load-path theme-dir)
   (add-to-list 'load-path theme-dir))
 
 (my/sync-theme-with-system)
 
-(setq custom-file (locate-user-emacs-file "custom-vars.el"))
-(load custom-file 'noerror 'nomessage)
+  ;;;; Nerd Icons
+(use-package nerd-icons
+  :ensure t)
+(use-package nerd-icons-completion
+  :ensure t
+  :after marginalia
+  :config
+  (nerd-icons-completion-mode)
+  (add-hook 'marginalia-mode-hook #'nerd-icons-completion-marginalia-setup))
+(use-package nerd-icons-corfu
+  :ensure t
+  :after corfu
+  :config
+  (add-to-list 'corfu-margin-formatters #'nerd-icons-corfu-formatter))
+(use-package nerd-icons-dired
+  :ensure t
+  :hook
+  (dired-mode . nerd-icons-dired-mode))
+
+  ;;; Minibuffer improvements
+(use-package vertico
+  :ensure t
+  :custom
+  (vertico-cycle t) ;; Enable cycling for `vertico-next/previous'
+  :hook
+  (after-init . vertico-mode))
+
+(use-package marginalia
+  :ensure t
+  ;; Bind `marginalia-cycle' locally in the minibuffer.  To make the binding
+  ;; available in the *Completions* buffer, add it to the
+  ;; `completion-list-mode-map'.
+  :bind
+  (:map minibuffer-local-map
+        ("M-A" . marginalia-cycle))
+  :hook
+  (after-init . marginalia-mode))
+
+;; Persist history over Emacs restarts. Vertico sorts by history position.
+(use-package savehist
+  :ensure nil
+  :hook (after-init . savehist-mode))
+
+(use-package orderless
+  :ensure t
+  :config
+  (setq completion-styles '(orderless basic))
+  (setq completion-category-defaults nil)
+  (setq completion-category-overrides nil)
+  (setq completion-pcm-leading-wildcard nil)) ;; Emacs 31: partial-completion behaves like substring
+
+  ;;; Completion
+(use-package corfu
+  :ensure t
+  :hook (after-init . global-corfu-mode)
+  :bind (:map corfu-map ("<tab>" . corfu-complete))
+  :custom
+  (tab-always-indent 'complete)
+  (corfu-preview-current nil)
+  (corfu-min-width 20)
+
+  (corfu-popupinfo-delay '(1.25 . 0.5))
+  (corfu-popupinfo-mode 1) ; shows documentation after `corfu-popupinfo-delay'
+
+  :config
+  ;; Sort by input history (no need to modify `corfu-sort-function').
+  (with-eval-after-load 'savehist
+    (corfu-history-mode 1)
+    (add-to-list 'savehist-additional-variables 'corfu-history)))
+
+(use-package goggles
+  :hook ((prog-mode text-mode) . goggles-mode)
+  :config
+  (setq-default goggles-pulse t)) ;; set to nil to disable pulsing
+
+;;; Dired
+(use-package dired
+  :ensure nil
+  :straight (:type built-in)
+  :commands (dired)
+  :hook
+  ((dired-mode . dired-hide-details-mode)
+   (dired-mode . hl-line-mode))
+  :custom
+  (dired-recursive-copies  'always)
+  (dired-recursive-deletes 'always)
+  (delete-by-moving-to-trash t)
+  (dired-dwim-target t))
+
+(use-package dired-subtree
+  :ensure t
+  :after dired
+  :bind
+  ( :map dired-mode-map
+    ("<tab>"     . dired-subtree-toggle)
+    ("TAB"       . dired-subtree-toggle)
+    ("<backtab>" . dired-subtree-remove)
+    ("S-TAB"     . dired-subtree-remove))
+  :custom
+  (dired-subtree-use-backgrounds nil))
+
+(use-package trashed
+  :ensure t
+  :commands (trashed)
+  :custom
+  (trashed-action-confirmer 'y-or-n-p)
+  (trashed-use-header-line t)
+  (trashed-sort-key '("Date deleted" . t))
+  (trashed-date-format "%Y-%m-%d %H:%M:%S"))
+
 (setq backup-directory-alist
       `(("." . ,(expand-file-name "backups/" user-emacs-directory)))
       auto-save-file-name-transforms
@@ -64,9 +237,6 @@
 (display-battery-mode 1)
 ;; This setting is suggested by the exwm docs but doesn't seem to play nice with vertico
 ;; (icomplete-vertical-mode 1)
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
 (pixel-scroll-precision-mode 1)
 (global-auto-revert-mode 1)
 (recentf-mode 1)
@@ -77,23 +247,6 @@
 (use-package eat)
 (use-package vterm)
 (use-package vundo)
-
-;; Enable Vertico.
-(use-package vertico
-  :custom
-  ;; (vertico-scroll-margin 0) ;; Different scroll margin
-  ;; (vertico-count 20) ;; Show more candidates
-  ;; (vertico-resize t) ;; Grow and shrink the Vertico minibuffer
-     (vertico-cycle t) ;; Enable cycling for `vertico-next/previous'
-  :init
-  (vertico-mode t))
-
-;; Persist history over Emacs restarts. Vertico sorts by history position.
-(use-package savehist
-  :init (savehist-mode t))
-
-(use-package delsel
-  :config (delete-selection-mode 1))
 
 ;; Emacs minibuffer configurations.
 (use-package emacs
@@ -111,31 +264,9 @@
   (minibuffer-prompt-properties
    '(read-only t cursor-intangible t face minibuffer-prompt)))
 
-;; Enable rich annotations using the Marginalia package
-(use-package marginalia
-  ;; Bind `marginalia-cycle' locally in the minibuffer.  To make the binding
-  ;; available in the *Completions* buffer, add it to the
-  ;; `completion-list-mode-map'.
-  :bind (:map minibuffer-local-map
-         ("M-A" . marginalia-cycle))
 
-  ;; The :init section is always executed.
-  :init
 
-  ;; Marginalia must be activated in the :init section of use-package such that
-  ;; the mode gets enabled right away. Note that this forces loading the
-  ;; package.
-  (marginalia-mode))
 
-(use-package orderless
-  :custom
-  ;; Configure a custom style dispatcher (see the Consult wiki)
-  ;; (orderless-style-dispatchers '(+orderless-consult-dispatch orderless-affix-dispatch))
-  ;; (orderless-component-separator #'orderless-escapable-split-on-space)
-  (completion-styles '(orderless basic))
-  (completion-category-overrides '((file (styles partial-completion))))
-  (completion-category-defaults nil) ;; Disable defaults, use our settings
-  (completion-pcm-leading-wildcard t)) ;; Emacs 31: partial-completion behaves like substring
 
 
 ;; Example configuration for Consult
@@ -275,24 +406,44 @@
 
 (use-package doom-modeline
   :init (doom-modeline-mode 1))
-(use-package nerd-icons)
 
-(set-face-attribute 'default        nil :font "Lilex Nerd Font"            :weight 'normal :height 100)
-(set-face-attribute 'fixed-pitch    nil :font "Lilex Nerd Font"            :weight 'normal :height 1.0)
-(set-face-attribute 'variable-pitch nil :font "Atkinson Hyperlegible Next" :weight 'normal :height 1.2)
+(use-package org-modern
+  :hook
+  (org-mode . org-modern-mode)
+  (org-agenda-finalize . org-modern-agenda)
+  :custom
+  (org-modern-fold-stars '(("⏵ " . "⏷ ")))
 
-;;; TODO: replace olivetti with visual-fill-column
+  (org-auto-align-tags nil)
+  (org-tags-column 0)
+  (org-catch-invisible-edits 'show-and-error)
+  (org-special-ctrl-a/e t)
+  (org-insert-heading-respect-content t)
+
+  (org-hide-emphasis-markers t)
+  (org-pretty-entities t)
+  (org-agenda-tags-column 0)
+  (org-ellipsis " …"))
+
+
+(use-package mixed-pitch
+  :hook
+  (text-mode . mixed-pitch-mode))
+
 (use-package olivetti
-  :hook ((markdown-mode . olivetti-mode)
-         (org-mode . olivetti-mode)
-         (pi-coding-agent-chat-mode . olivetti-mode)
-         (pi-coding-agent-input-mode . olivetti-mode)))
+  :custom
+  (olivetti-minimum-body-width 80)
+  (olivetti-recall-visual-line-mode-entry-state t)
+  :config
+  (setq-default olivetti-body-width 0.7)
+  :hook ((text-mode . olivetti-mode)))
+
 
 (use-package meow
   :config (require 'meow-config))
 
 (use-package pi-coding-agent
- :config (defalias 'pi 'pi-coding-agent))
+  :config (defalias 'pi 'pi-coding-agent))
 
 ;; Keep EXWM installed, but only start it from the EXWM desktop session.
 (use-package exwm
